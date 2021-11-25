afcd3d110bd93cf3514c2a611f3f122c2ff0b90a
Your Name <sehamerhoma@gmail.com>
2021-11-25 12:46:07 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import prettyFormat from 'pretty-format';
+import FullName from './component/Profile/FullName';
+import Address from './component/Profile/Address';
+import Profilephoto from './component/Profile/ProfilePhoto';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br/>
+
+      <Profilephoto/>
+      <br/>
+      <FullName/>
+      <br/>
+      <Address/>
     </div>
   );
 }
